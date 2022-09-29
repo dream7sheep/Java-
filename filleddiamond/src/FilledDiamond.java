015864e4ccea0a4494cb0ee011f6c77f0c67538f
dream7sheep <107824480+dream7sheep@users.noreply.github.com>
2022-09-29 22:16:33 +0800

    filleddiamond
    用#画一个实心菱形

---
+++ filleddiamond/src/FilledDiamond.java
@@ -0,0 +1,26 @@
+public class FilledDiamond {
+
+    public static void main(String[] args) {
+        int size=5;
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print('#');
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= size-1; i++) {
+            for (int j = 1; j <= i; j++){
+                System.out.print(" ");
+            }
+
+            for (int k = 2*size-3; k >= 2 * i - 1; k--){
+                System.out.print('#');
+            }
+            System.out.println();
+        }
+    }
+}
